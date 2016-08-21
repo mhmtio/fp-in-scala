d15898f08336e87a370cf9d69b80aa553c5047f7
Matthias Hanitzsch-Moonlight <mhm@terrafino.io>
2016-08-21 17:37:18 +0100

    Tailrec Fibonacci numbers

---
+++ build.sbt
@@ -0,0 +1,5 @@
+name := "fp-in-scala"
+
+version := "1.0"
+
+scalaVersion := "2.11.7"
---
+++ src/main/scala/io/terrafino/fp/Fibonacci.scala
@@ -0,0 +1,22 @@
+package io.terrafino.fp
+
+object Fibonacci extends App {
+
+  def fib1(n: Int): Int = {
+    if (n == 0 || n == 1) n
+    else fib1(n - 1) + fib1(n - 2)
+  }
+
+  def fib2(n: Int): Int = {
+    @annotation.tailrec
+    def go(n: Int, prev: Int, curr: Int): Int = {
+      if (n == 0) prev
+      else go(n - 1, curr, prev + curr)
+    }
+    go(n, 0, 1)
+  }
+
+  for (i <- 0 to 10) {
+    println(s"$i : ${fib1(i)} : ${fib2(i)}")
+  }
+}
