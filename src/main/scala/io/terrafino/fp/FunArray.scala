3579e0a67e6f5882a123b18271dede599f1a6cd2
Matthias Hanitzsch-Moonlight <mhm@terrafino.io>
2016-08-21 17:55:50 +0100

    Polymorphic isSorted.

---
+++ src/main/scala/io/terrafino/fp/FunArray.scala
@@ -0,0 +1,15 @@
+package io.terrafino.fp
+
+object FunArray extends App {
+  def isSorted[A](as: Array[A], ordered: (A, A) => Boolean): Boolean = {
+    def loop(n: Int): Boolean = {
+     if (n+1 >= as.size) true
+     else if (!ordered(as(n), as(n+1))) false
+     else loop(n+1)
+    }
+    loop(0)
+  }
+  
+  val res = isSorted(Array(1,2,3,4,1), (a: Int, b: Int) => a <= b)
+  println(res)
+}
