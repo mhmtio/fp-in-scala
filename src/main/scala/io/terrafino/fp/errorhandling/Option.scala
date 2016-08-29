6550ef598a499d72c969bcaaecfa4bc34710eff8
Matthias Hanitzsch-Moonlight <mhm@terrafino.io>
2016-08-29 12:33:58 +0100

    Amended Option and added Either.

--- src/main/scala/io/terrafino/fp/errorhandling/Option.scala
+++ src/main/scala/io/terrafino/fp/errorhandling/Option.scala
@@ -5,11 +5,11 @@ import scala.{ Option => _, Some => _, Either => _, _ } // hide std library `Opt
 sealed trait Option[+A] {
   def map[B](f: A => B): Option[B] = this match {
     case Some(a) => Some(f(a))
-    case None => None
+    case None    => None
   }
   def getOrElse[B >: A](default: => B): B = this match {
     case Some(a) => a
-    case None => default
+    case None    => default
   }
   def flatMap[B](f: A => Option[B]): Option[B] = {
     map(f(_)) getOrElse None
@@ -22,7 +22,6 @@ sealed trait Option[+A] {
     flatMap(a => if (f(a)) Some(a) else None)
   }
 
-
 }
 case class Some[+A](get: A) extends Option[A]
 case object None extends Option[Nothing]
@@ -33,16 +32,31 @@ object Option {
     else Some(xs.sum / xs.length)
 
   def map2[A, B, C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] = {
-    a flatMap(aa => b map (bb => f(aa, bb)))
+    a flatMap (aa => b map (bb => f(aa, bb)))
   }
 
   def variance(xs: Seq[Double]): Option[Double] =
-    mean(xs) flatMap (m => mean(xs.map (x => math.pow(x-m, 2))))
+    mean(xs) flatMap (m => mean(xs.map(x => math.pow(x - m, 2))))
 
   def sequence[A](a: List[Option[A]]): Option[List[A]] = a match {
-      case Nil => Some(Nil)
-      case h::t => h flatMap(hh => sequence(t) map (hh :: _))
-    }
+    case Nil    => Some(Nil)
+    case h :: t => h flatMap (hh => sequence(t) map (hh :: _))
+  }
+
+  def sequence2[A](a: List[Option[A]]): Option[List[A]] =
+    traverse(a)(a => a)
+
+  // val ofh: Option[B] = f(h)
+  // val x: Option[List[B]] = ofh flatMap ((b: B) => traverse(t)(f))
+  // val res = x map ((l: List[B]) => ofh :: l)
+  def traverse[A, B](a: List[A])(f: A => Option[B]): Option[List[B]] = a match {
+    case Nil    => Some(Nil)
+    case h :: t => f(h) flatMap (fhh => traverse(t)(f) map (fhh :: _))
+  }
+
+  def Try[A](a: => A): Option[A] =
+    try { Some(a) }
+    catch { case e: Exception => None }
 
 }
 
